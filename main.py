1595dfc1c1b1eef51f0dee1ea7222bfe3143dd24
hyunkang <aoaoaoahah@gmail.com>
2020-12-11 05:33:16 +0900

    weather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, url_for
-app = Flask(__name__)
+from urllib.request import urlopen
+import json
 
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -10,7 +12,14 @@ def home():
 @app.route('/result',methods = ['POST'])
 def result():
    location=request.form['location']
-   return render_template('result.html', location=location)
+   url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&mode=json&APPID=ad91812895dfa17a92a11504c0718122'
+   data = urlopen(url).read()
+   a = json.loads(data)
+   we = (a['weather'][0]['main'])
+
+   ce = round(a['main']['temp'] - 273.15, 2)
+
+   return render_template('result.html',we=we, ce=ce)
 
 
 if __name__ == '__main__':
