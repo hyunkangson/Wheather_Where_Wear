c82a1fbe89f66c964140615cf6b5d4788aa3fd62
hyunkang <aoaoaoahah@gmail.com>
2020-12-11 05:39:58 +0900

    templates + button + weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
-import urllib.request
-import urllib.parse
+from flask import Flask, render_template, request, url_for
+from urllib.request import urlopen
+import json
 
 app = Flask(__name__)
 
@@ -9,12 +9,18 @@ def home():
     return render_template('home.html');
 
 
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/result',methods = ['POST'])
 def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
+   location=request.form['location']
+   url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&mode=json&APPID=ad91812895dfa17a92a11504c0718122'
+   data = urlopen(url).read()
+   a = json.loads(data)
+   we = (a['weather'][0]['main'])
+
+   ce = round(a['main']['temp'] - 273.15, 2)
+
+   return render_template('result.html',we=we, ce=ce)
+
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.2', debug = True)
