2b8ac278d319c80f6bb8898b4537a88c80a631b8
hyunkang <aoaoaoahah@gmail.com>
2020-12-12 23:20:31 +0900

    DB connection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, url_for
+from flask import request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 from urllib.request import urlopen
-import json
+import json, sqlite3
 
 app = Flask(__name__)
 
@@ -11,6 +13,7 @@ def home():
 
 @app.route('/result',methods = ['POST'])
 def result():
+    #weather api
    location=request.form['location']
    url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&mode=json&APPID=ad91812895dfa17a92a11504c0718122'
    data = urlopen(url).read()
@@ -19,7 +22,27 @@ def result():
    ce = round(a['main']['temp'] - 273.15, 2)
    icon = a['weather'][0]['icon']
 
-   return render_template('result.html',we=we, ce=ce, icon=icon)
+   #database
+   con=sqlite3.connect("cloth.db")
+   cursor = con.cursor()
+   if ce >= 27:
+        query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=1 and is'
+   elif ce >= 23 and ce <= 26:
+        query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=2'
+   elif ce >= 20 and ce <= 22:
+       query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=3'
+   elif ce >= 17 and ce <= 19:
+       query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=4'
+   elif ce >= 12 and ce <= 16:
+       query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=5'
+   elif ce >= 10 and ce <= 11:
+       query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=6'
+   elif ce >= 6 and ce <= 9:
+       query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=7'
+   else: query='SELECT outer0, top, bottom, etc FROM cloth WHERE tno=8'
+   item = cursor.execute(query).fetchall()
+
+   return render_template('result.html',we=we, ce=ce, icon=icon,location=location,item=item)
 
 
 if __name__ == '__main__':
